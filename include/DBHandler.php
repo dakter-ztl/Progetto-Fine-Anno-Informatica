5dff71a86ea17a03da023b4a64dbe95f4cb39fb3
dakter-ztl <victor.mereuta2@gmail.com>
2026-04-24 11:24:23 +0200

    cambiamento sicurezza database, creazione config.php personale e tolta di credenziali dal file publico db

--- include/DBHandler.php
+++ include/DBHandler.php
@@ -1,27 +1,21 @@
 <?php
-class DBHandler {
+require_once __DIR__ . '/../config.php';
 
-private static $host = 'pronv6.ftp.tb-hosting.com'; 
-private static $dbName = 'pronv6_zwjejm53'; 
-private static $userName = 'pronv6_dakter';       
-private static $password = 'ciscoX123'; 
+class DBHandler {
     private static $dbHandler = null;
 
-    public static function getPDO(){
-        if(self::$dbHandler == null){
+    public static function getPDO() {
+        if (self::$dbHandler == null) {
             try {
-                $dsn = "mysql:host=" . self::$host . ";dbname=" . self::$dbName . ";charset=utf8";
-                
+                $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8";
                 $options = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES => false,
                 ];
-
-                self::$dbHandler = new PDO($dsn, self::$userName, self::$password, $options);
-                
+                self::$dbHandler = new PDO($dsn, DB_USER, DB_PASS, $options);
             } catch (PDOException $e) {
-                die("errore connessione: " . $e->getMessage()); 
+                die("Errore connessione DB"); 
             }
         }
         return self::$dbHandler;
