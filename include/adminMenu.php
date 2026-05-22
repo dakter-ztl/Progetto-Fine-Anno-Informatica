6672a7622fec31a567c297feff8f6b50e800a23b
dakter <victor.mereuta2@gmail.com>
2026-05-22 12:11:56 +0200

    -fix completo delle path, ho dovuto usare le Globals per poter reindirizzare l'utente tra le varie cartelle userPages e adminPages

--- include/adminMenu.php
+++ include/adminMenu.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-    require_once '../include/userMenu.php';
 
-if(isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'admin'){
-    require_once '../include/menuChoice.php';
-    exit;
+if(!isset($_SESSION['ruoloUtente']) || $_SESSION['ruoloUtente'] !== 'admin'){
+    header("Location: ../userPages/home.php");
+    exit();
 }
+require_once '../include/menuChoice.php';
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +14,7 @@ if(isset($_SESSION['ruolo']) && $_SESSION['ruolo'] == 'admin'){
     <meta charset="UTF-8">
     <title>Dashboard Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 
