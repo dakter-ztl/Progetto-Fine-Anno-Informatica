93f8d7e7559e23c78391424dde647b2436f6d3a0
dakter <victor.mereuta2@gmail.com>
2026-05-25 20:19:37 +0200

    tolto codice di debug

--- userPages/home.php
+++ userPages/home.php
@@ -10,9 +10,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require_once '../include/DBHandler.php';
 include '../include/menuChoice.php';
